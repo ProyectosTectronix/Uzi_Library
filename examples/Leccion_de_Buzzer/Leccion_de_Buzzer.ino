1dad768d1ad55b5d955a9531af47e7cd1c2c7f20
jonathanjerez <35068936+jonathanjerez@users.noreply.github.com>
2018-02-07 09:27:35 -0300

    Update Leccion_de_Buzzer.ino
    se tradujo comentario de del codigo

--- examples/Leccion_de_Buzzer/Leccion_de_Buzzer.ino
+++ examples/Leccion_de_Buzzer/Leccion_de_Buzzer.ino
@@ -1,21 +1,21 @@
 /**
- * Ejemplo para la Lección de Buzzer.
+ * Example for the Buzzer lesson.
  */
-#include <macros.h> // Archivo de cabecera que incluye todas las macros utilizadas por la plataforma robótica UZI.
-#include <Buzzer.h> // Archivo de cabecera en donde se define la clase Buzzer
+#include <macros.h> // Header file that includes all the macros used by the UZI robotic platform.
+#include <Buzzer.h> // Header file that define the Buzzer class.
 
-Buzzer prueba_de_buzzer = Buzzer(); // Instanciación de un Buzzer llamando a su constructor.
+Buzzer prueba_de_buzzer = Buzzer(); // Instantation of a Buzzer by calling its constructor.
 
 void setup() {
   // put your setup code here, to run once:
-  prueba_de_buzzer.init(); // Sonara un LA 440 por 500 ms para verificar correcto funcionamiento.
+  prueba_de_buzzer.init(); // will sound a LA 440 for 500 ms for check  correct operation. 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  prueba_de_buzzer.sound(DO, 1, 250); // DO_ para un DO#.
-  prueba_de_buzzer.sound(RE, 1, 250); // RE_ para un RE#.
-  prueba_de_buzzer.sound(MI, 1, 250); // sonara un MI por 250 milisegundos.
+  prueba_de_buzzer.sound(DO, 1, 250); // DO_ for a DO#.
+  prueba_de_buzzer.sound(RE, 1, 250); // RE_ for a RE#.
+  prueba_de_buzzer.sound(MI, 1, 250); // will sound a MI for 250 ms.
   prueba_de_buzzer.sound(FA, 1, 250);
   prueba_de_buzzer.sound(SOL, 1, 250);
   prueba_de_buzzer.sound(LA, 1, 250);
