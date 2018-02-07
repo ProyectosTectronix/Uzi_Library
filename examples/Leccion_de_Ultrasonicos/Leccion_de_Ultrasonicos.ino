d98af3c0d565f05b3140ec6bcc61f33ebaac8012
jonathanjerez <35068936+jonathanjerez@users.noreply.github.com>
2018-02-07 11:46:04 -0300

    Update Leccion_de_Ultrasonicos.ino
    se traducen comentarios del codigo

--- examples/Leccion_de_Ultrasonicos/Leccion_de_Ultrasonicos.ino
+++ examples/Leccion_de_Ultrasonicos/Leccion_de_Ultrasonicos.ino
@@ -2,22 +2,22 @@
 #include <Ultrasonic_UZI.h>
 
 Ultrasonic_UZI prueba_de_sonico = Ultrasonic_UZI();
-int lectura_sensor_izq = 0; // variable para guardar las lecturas del sensor izquierdo.
-int lectura_sensor_der = 0; // variables para guardar las lecturas del sensor derecho.
+int lectura_sensor_izq = 0; // variable to save the left sensor readings.
+int lectura_sensor_der = 0; // variable to save the right sensor readings.
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200); // Se inicia el puerto serial para visualizar las lecturas por el Serial Monitor de esta IDE.
-  prueba_de_sonico.init(); // Se inicializan los sensores ultrasónicos.
+  Serial.begin(115200); // The serial port is started to view the readings by the Serial Monitor of this IDE.
+  prueba_de_sonico.init(); // Ultrasonic sensors are initialized.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  lectura_sensor_izq = prueba_de_sonico.read(0 /* sensor de la izquierda */, 30 /* ignorar medicion de objetos mas lejanos que 30 cm */); // Se hace la lectura del sensor de la izquierda
-  delay(20);                // Se recomienda usar un delay entre las lecturas de cada sensor,
-                            // ya que la señal de triger de uno puede ser recibida por el echo
-                            // del otro obteniendo mediciones outliers.
-                            // Se usa 20 ms ya que es el tiempo estimado de lectura a máximo rango.
+  lectura_sensor_izq = prueba_de_sonico.read(0 /* sensor on the left */, 30 /* ignore measurement of objects farther than 30 cm */); // The sensor on the left is read
+  delay(20);                // It is recommended to use a delay between the readings of each sensor,
+                            // since the triger signal of one can be received by the ECHO
+                            // From the other obtaining outliers measurements.
+                            // 20 ms is used since it is the estimated time of reading at maximum rank.
   lectura_sensor_der = prueba_de_sonico.read(1, 30); // Se hace la lectura del sensor de la derecha.
   delay(20);
   Serial.print("lectura raw de la izquierda : " + lectura_sensor_izq + "\t\t");
