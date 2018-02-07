47860ab6a610b5dd4492f0fb20d4216019c882e8
jonathanjerez <35068936+jonathanjerez@users.noreply.github.com>
2018-02-07 10:07:25 -0300

    Update Leccion_de_Qre.ino
    se tradice comentarios del codigo

--- examples/Leccion_de_Qre/Leccion_de_Qre.ino
+++ examples/Leccion_de_Qre/Leccion_de_Qre.ino
@@ -1,21 +1,20 @@
-#include <macros.h> // Archivo con todas las macros de la plataforma Uzi
-#include <Qre.h>    // Archivo con las definiciones de un objeto Qre.
+#include <macros.h> // File with all macros of the Uzi platform.
+#include <Qre.h>    // File with the definitions of the object Qre.
 
-Qre prueba_de_qre = Qre(); // Instanciación de la Creación de  un objeto del tipo Qre usando su contructor por defecto: Qre().
-int *qres; // Variable que guardará el estado lógico de cada Qre.
-
+Qre prueba_de_qre = Qre(); // Instantiation of the creation of a Qre object using its default constructor.
+int *qres; // Variable that will save the logical state of each Qre.
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200); // Se establece comunicación serial para ver el estado de los Qre por el Serial Monitor de este IDE.
-  prueba_de_qre.init(); // Se inicializan los Qre.
+  Serial.begin(115200); // Serial communication is established to see the status of the Qre by the Serial Monitor of this IDE.
+  prueba_de_qre.init(); // The Qre are initialized.
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  qres = prueba_de_qre.read(); // Se lee estado de los 5 Qre
+  qres = prueba_de_qre.read(); // It reads state of the 5 Qre
 
   /**
-   * Se escriben los valores de cada Qre en el puerto serial.
+   * The values of each Qre are written to the serial port.
    */
   String tmp = "";
   for(int i=0; i<5; i++){
