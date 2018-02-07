7ad13a6a0ef56151d67937ad35b013abe8b35e3e
jonathanjerez <35068936+jonathanjerez@users.noreply.github.com>
2018-02-07 10:13:18 -0300

    Update Leccion_de_Seguimiento_de_Linea_ON_OFF.ino
    se traducen comentarios del codigo

--- examples/Leccion_de_Seguimiento_de_Linea_ON_OFF/Leccion_de_Seguimiento_de_Linea_ON_OFF.ino
+++ examples/Leccion_de_Seguimiento_de_Linea_ON_OFF/Leccion_de_Seguimiento_de_Linea_ON_OFF.ino
@@ -2,36 +2,35 @@
 #include <Qre.h>
 #include <Motor.h>
 
-Motor motores = Motor(); // Creación del Objeto Motor.
-Qre linea = Qre(); // Creación del Objeto Qre.
+Motor motores = Motor(); //Creation of the Motor Object.
+Qre linea = Qre(); // Creation of the Qre Object.
 
-int qres[5]; // Variable que guardará el estado de los 5 Qres
-
+int qres[5]; // Variable that will save the state of the 5 Qres
 void setup() {
   // put your setup code here, to run once:
-  motores.init(); // Inicialización de los motores
-  linea.init(); // Inicialización de los Qres
+  motores.init(); // Initialization of the motors
+  linea.init(); // Initialization of the Qres
 }
 
 /**
- * A continuación se muestra una rutina sensilla de como seguir una linea negra sobre una superficie lisa y blanca.
+ * Below is a simple routine of how to follow a black line on a smooth white surface.
  */
 void loop() {
   // put your main code here, to run repeatedly:
-  qres = linea.read(); // Se lee el estado de los Qres.
-  if((qres[0] == 0) && (qres[1] == 0) && (qres[2] == 0) && (qres[3] == 0) && (qres[4] == 0)){ // Si no hay linea avanza.
+  qres = linea.read(); // The state of the Qres is read.
+  if((qres[0] == 0) && (qres[1] == 0) && (qres[2] == 0) && (qres[3] == 0) && (qres[4] == 0)){ // If there is no line, it advances.
     motores.forward(50);  
   }
-  else if(qres[2] == 1){ // Si hay linea al centro avanza recto.
+  else if(qres[2] == 1){ // If there is a line to the center, go straight.
     motores.forward(50);  
   }
-  else if(qres[0] == 1){ // Si hay linea a la extrema izquierda avanza girando a la derecha.
+  else if(qres[0] == 1){ // If there is a line to the far left, move forward to the right.
     motores.differentialFWD(50,100);  
   }
-  else if(qres[4] == 1){ // Si hay linea a la extrema derecha avanza girando a la izquierda.
+  else if(qres[4] == 1){ // If there is a line to the far right, move forward to the left.
     motores.differentialFWD(100,50);
   }
   else{
-    motores.stop(0);  // Si no se cumple ningun caso, detente.
+    motores.stop(0);  // If no case is met, stop.
   }
 }
