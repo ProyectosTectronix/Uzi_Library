39d6561c13a7990387475513c79dfe30897e7a90
Tectronix <proyectos@tectronix.cl>
2018-02-26 16:40:09 -0300

    Update Buzzer.ino
    Se mueve la escala 3 octavas mas arriba para una mejor reproduccion del audio.

--- examples/Leccion_de_Buzzer/Leccion_de_Buzzer.ino
+++ examples/Leccion_de_Buzzer/Leccion_de_Buzzer.ino
@@ -9,14 +9,14 @@ Buzzer prueba_de_buzzer = Buzzer(); // Instantation of a Buzzer by calling its c
 void setup() {
   // put your setup code here, to run once:
   prueba_de_buzzer.init(); // Sonara un LA 440 por 500 ms para verificar correcto funcionamiento.
-  prueba_de_buzzer.sound(DO, 4, 250); // DO_ for a DO#.
-  prueba_de_buzzer.sound(RE, 4, 250); // RE_ for a RE#.
-  prueba_de_buzzer.sound(MI, 4, 250); // will sound a MI for 250 ms.
-  prueba_de_buzzer.sound(FA, 4, 250);
-  prueba_de_buzzer.sound(SOL, 4, 250);
-  prueba_de_buzzer.sound(LA, 4, 250);
-  prueba_de_buzzer.sound(SI, 4, 250);
-  prueba_de_buzzer.sound(DO, 5, 250);
+  prueba_de_buzzer.sound(DO, 7, 250); // DO_ for a DO#.
+  prueba_de_buzzer.sound(RE, 7, 250); // RE_ for a RE#.
+  prueba_de_buzzer.sound(MI, 7, 250); // will sound a MI for 250 ms.
+  prueba_de_buzzer.sound(FA, 7, 250);
+  prueba_de_buzzer.sound(SOL, 7, 250);
+  prueba_de_buzzer.sound(LA, 7, 250);
+  prueba_de_buzzer.sound(SI, 7, 250);
+  prueba_de_buzzer.sound(DO, 8, 250);
 }
 
 void loop() {
